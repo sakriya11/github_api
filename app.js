42e6aacadd7e8bec3c59e68e82176d119179746b
Sakriya Khadka <Khadkasakriya81@gamil.com>
2021-11-15 21:04:22 +0545

    required data from git api fetched

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const api_route = require("./router/apiRoute");
+
+app.use(cors());
+app.use(api_route);
+
+app.listen(3000, function () {
+  console.log("The server is runnning");
+});
---
+++ router/apiRoute.js
@@ -0,0 +1,32 @@
+const axios = require("axios");
+const express = require("express");
+const circularJson = require("circular-json");
+const router = express.Router();
+
+router.get("/api", async function (req, res) {
+  try {
+    const url = "https://api.github.com/search/repositories?q=stars:>100000";
+
+    let a = await axios.get(url);
+
+    let data = JSON.parse(circularJson.stringify(a)).data;
+    let filter = [];
+    for (var i of data.items) {
+      let newObj = {};
+      newObj["repository_name"] = i.name;
+      newObj["author"] = i.full_name;
+      newObj["no_of_star"] = i.watchers_count;
+      newObj["watcher"] = i.watchers_count;
+      newObj["forks"] = i.fork;
+      newObj["desc"] = i.description;
+      newObj["last_update"] = i.updated_at;
+
+      filter.push(newObj);
+    }
+    return res.status(200).json({ a: filter });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+module.exports = router;
